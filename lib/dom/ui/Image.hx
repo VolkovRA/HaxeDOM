978076f103f53e850fade6f2ff2aab8868ee09ca
VolkovRA <flash21.ru@gmail.com>
2020-12-18 22:11:24 +0300

    Добавлен Image

---
+++ lib/dom/ui/Image.hx
@@ -0,0 +1,72 @@
+package dom.ui;
+
+import js.Browser;
+import js.lib.Error;
+import js.html.ImageElement;
+import dom.display.Component;
+import dom.utils.Dispatcher;
+
+/**
+ * Картинка.  
+ * В DOM представлена тегом: `<img>`
+ */
+class Image extends Component<Image, ImageElement>
+{
+    /**
+     * Создать обычное, текстовое поле.
+     * @param src URL Адрес графического файла.
+     */
+    public function new(?src:String) {
+        super(Browser.document.createImageElement());
+        this.node.classList.add("image");
+
+        if (src != null)
+            this.src = src;
+    }
+
+    /**
+     * URL Адрес графического файла.  
+     * По умолчанию: `null`
+     */
+    public var src(default, set):String = null;
+    function set_src(value:String):String {
+        if (value == src)
+            return value;
+
+        src = value;
+        node.src = value;
+        return value;
+    }
+
+    /**
+     * Событие завершения загрузки изображения.  
+     * Всегда вызывается один раз после завершения загрузки.
+     * В случае ошибки передаётся объект: `js.lib.Error`
+     * 
+     * Не может быть: `null`
+     */
+    public var onLoad(get, null):Dispatcher<Error->Void>;
+    @:noCompletion
+    function get_onLoad():Dispatcher<Error->Void> {
+        if (onLoad == null) {
+            onLoad = new Dispatcher();
+            this.node.addEventListener("load", function(e) {
+                onLoad.emit(null);
+            });
+            this.node.addEventListener("error", function() {
+                onLoad.emit(new Error("Ошибка загрузки изображения: " + src));
+            });
+        }
+        return onLoad;
+    }
+
+    /**
+     * Получить текстовое описание объекта.
+     * @return Возвращает текстовое представление этого экземпляра.
+     */
+    @:keep
+    @:noCompletion
+    override public function toString():String {
+        return "[Image]";
+    }
+}
--- src/Main.hx
+++ src/Main.hx
@@ -1,9 +1,11 @@
 package;
 
-import js.Browser;
 import dom.display.Stage;
+import dom.ui.Image;
+import dom.ui.Button;
 import dom.ui.Label;
 import dom.utils.NativeJS;
+import js.Browser;
 
 class Main
 {   
@@ -11,7 +13,12 @@ class Main
         trace("Поддержка ResizeObserver: " + NativeJS.isResizeObserverSupported());
         var stage = new Stage(Browser.document.body);
 
-        var label = new Label("Label");
-        stage.addChild(label);
+        var lb = new Label("Label");
+        var img = new Image("http://www.flasher.ru/forum/images/russian/flasher_logo_2013.gif");
+        //var bt = new Button("Button");
+
+        stage.addChild(lb);
+        stage.addChild(img);
+        //stage.addChild(bt);
     }
 }
