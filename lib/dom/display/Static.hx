17097d04b95975d3e5809a2c115f50b41300ee2a
VolkovRA <flash21.ru@gmail.com>
2021-01-19 21:01:22 +0300

    Кешируемый контент в Static

--- lib/dom/display/Static.hx
+++ lib/dom/display/Static.hx
@@ -11,28 +11,54 @@ class Static extends Component
 {
     /**
      * Создать новый экземпляр.
-     * @param html HTML Контент.
+     * @param content HTML Контент.
      * @param node HTML DOM Элемент, представляющий этот объект. *(По умолчанию: `<div>`)*
      */
-    public function new(?html:String, ?node:Element) {
+    public function new(?content:String, ?node:Element) {
         super(node);
 
-        if (html != null)
-            this.html = html;
+        if (content != null)
+            this.content = content;
     }
 
     /**
-     * HTML Контент.  
-     * Это свойство полностью эквивалентно: `node.innerHTML`
+     * HTML Содержимое.  
+     * Свойство позволяет работать с содержимым элемента:
+     * `node.innerHTML` используя **кеширование**.
+     * 
+     * При передаче нового значения, оно сохраняется в этом
+     * элементе и обновляет DOM, только если было передано
+     * **новое** значение, отличное от ранее заданного. Это
+     * позволяет не манипулировать DOM, если фактические
+     * изменения отсустствуют.
+     * 
+     * Особенности:
+     * - Работает с нативным свойством: `innerHTML`
+     * - Кеширует указанные данные в памяти этого компонента.
+     * - Обновляет DOM, только если передано новое значение.
+     * - Считывание свойства происходит из кеша.
+     * - Не следит за изменениями в: `node.innerHTML` извне.
+     * - Не может быть: `null`
      * 
      * По умолчанию: `""`
      */
-    public var html(get, set):String;
-    inline function get_html():String {
-        return node.innerHTML;
-    }
-    inline function set_html(value:String):String {
-        node.innerHTML = value;
+    public var content(default, set):String = "";
+    function set_content(value:String):String {
+        if (content == value)
+            return value;
+
+        if (value == null) {
+            if (content == "")
+                return value;
+
+            content = "";
+            node.innerHTML = "";
+        }
+        else {
+            content = value;
+            node.innerHTML = value;
+        }
+
         return value;
     }
 }
